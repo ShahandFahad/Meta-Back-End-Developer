bf329fb336f8668b309daedc6c10ce7586746d08
ShahandFahad <shahandfahadmad@gmail.com>
2023-07-12 13:56:54 +0500

    Course 5 Final Graded Lab

---
+++ 5-Django-Web-Framework/Week-5/Final Django App/workplace/littlelemon/restaurant/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Booking, Menu
+
+# Register your models here.
+admin.site.register(Booking)
+admin.site.register(Menu)
---
+++ 5-Django-Web-Framework/Week-5/Final Django App/workplace/littlelemon/restaurant/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("book/", views.book, name="book"),
+    # Add the remaining URL path configurations here
+    path("menu/", views.menu, name="menu"),
+    path("menu_item/<int:pk>/", views.display_menu_items, name="menu_item"),
+]
