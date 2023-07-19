363a55cf0c0734024aae069ff623b27e4eaaeb57
ShahandFahad <shahandfahadmad@gmail.com>
2023-07-19 22:01:31 +0500

    Course Seven Week 5 Labs

---
+++ 7-The-Full-Stack/Week-5/Exercise 01 Connect the Little Lemon back-end to MySQL/myproject/myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Booking(models.Model):
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return self.first_name
---
+++ 7-The-Full-Stack/Week-5/Exercise 02 Set up a Little Lemon booking API/littlelemon/restaurant/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Menu
+from .models import Booking
+
+
+admin.site.register(Menu)
+admin.site.register(Booking)
